793fb7ad856392560a2f774f2aa18f2aae0607e5
HollyHilts <holly.a.hilts@gmail.com>
2021-05-12 15:36:48 -0400

    created flat model and added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Flat.create!(
+    name: "#{Faker::Hipster.word} #{Faker::House.room}",
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.review,
+    price_per_night: 75,
+    number_of_guests: 3
+  )
+end
